6d37148703bfb170b07b2029153075c9af2a4bb9
s18001-ikeshiro <s18001@std.it-college.ac.jp>
2018-07-14 17:35:24 +0900

    修正しました。よければ見てください

--- src/basic-c2/task/task20180711.py
+++ src/basic-c2/task/task20180711.py
@@ -4,12 +4,11 @@ paper = "パー"
 scissors = "チョキ"
 # CPUが必ず勝つように仕向ける
 zyanken = input("何を出しますか？1:グー、2:チョキ、3:パー")
-if zyanken == rock:
-    result = paper
-elif zyanken == paper:
-    result = scissors
-elif zyanken == scissors:
-    result = rock
+if zyanken == str(1):
+    print("CPUが", paper, "を出しました。あなたの負けです。")
+elif zyanken == str(2):
+    print("CPUが", rock, "を出しました。あなたの負けです。")
+elif zyanken == str(3):
+    print("CPUが", scissors, "を出しました。あなたの負けです。")
 else:
-    print("入力が不正です。終了します")
-print("CPUが", result, "を出しました。あなたの負けです。")
+    print("入力が不正です。終了します。")
