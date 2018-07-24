b20dabc0d86f9a54378679f5caace5f3d7420f0f
s18001-ikeshiro <s18001@std.it-college.ac.jp>
2018-07-24 13:31:17 +0900

    途中です。

---
+++ src/basic-c3/task/task20180719.py
@@ -0,0 +1,15 @@
+print("四則計算プログラムです。")
+parameters1 = input("第1パラメータを入力してください>>>")
+# whileが正しく書かれているかわかりません
+while parameters1.isnumeric():
+    if False:
+        print("error: 数値以外が入力されました。")
+    else:
+        break
+parameters2 = input("第2パラメータを入力してください>>>")
+while parameters2.isnumeric():
+    if True:
+        break
+    elif False:
+        print("error: 数値以外が入力されました。")
+input("演算方法を入力してください>>>")
