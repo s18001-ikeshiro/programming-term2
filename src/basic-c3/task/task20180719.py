fa8d4d5d755e2365eb5d1bfa4495fba7b6337eb5
s18001-ikeshiro <s18001@std.it-college.ac.jp>
2018-07-26 13:46:20 +0900

    途中です

--- src/basic-c3/task/task20180719.py
+++ src/basic-c3/task/task20180719.py
@@ -1,15 +1,8 @@
 print("四則計算プログラムです。")
-parameters1 = input("第1パラメータを入力してください>>>")
-# 書いた後に気づいたのですが多分while使いませんね。
-while parameters1.isnumeric():
-    if False:
-        print("error: 数値以外が入力されました。")
+argument1 = input("第1パラメータを入力してください>>>")
+argument2 = input("第2パラメータを入力してください>>>")
+operator = input("演算方法を入力してください>>>")
+while True:
+    if operator == "+" or "-" or "*" or "/":
+        break
     else:
-        break
-parameters2 = input("第2パラメータを入力してください>>>")
-while parameters2.isnumeric():
-    if True:
-        break
-    elif False:
-        print("error: 数値以外が入力されました。")
-input("演算方法を入力してください>>>")
