01dba79d5d23231b24d4db072d33039a70483010
s18001-ikeshiro <s18001@std.it-college.ac.jp>
2018-07-26 13:41:37 +0900

    最低限やりました

--- src/basic-c3/task/task20180718.py
+++ src/basic-c3/task/task20180718.py
@@ -1,20 +1,23 @@
 # 辞書型のデータ(メインメニューと値段)を変数に代入
-main = {
+main_menus = {
     "DripCoffee": 280, "ColdBrewCoffee": 320,
-    "CafeLatte": 330, "SoyLatte": 380, "Cappuccino": 330,
-    "CaramelFrappuccino": 470, "MacchaCreamFrappuccino": 470
+    "CafeLatte": 330, "SoyLatte": 380,
+    "Cappuccino": 330, "CaramelFrappuccino": 470,
+    "MacchaCreamFrappuccino": 470
 }
 
 # 辞書型のデータ(オブションメニューと値段)を変数に代入
-option = {
+option_menus = {
     "LowFatMilk": 0, "NonFatMilk": 0, "SoyMilk": 50,
     "DeepCoffee": 60, "WhipCream": 70, "CaramelShrup": 60,
     "ChocoSource": 0, "DeCafe": 50
 }
-
 while True:
     customer = input("メインメニューを選んでください")
     if customer == "" or customer == "q":
         break
+    elif customer in main_menus:
+        print(main_menus[customer])
+        break
     else:
-            input("メインメニューを承りました")
+        print("そのような商品はありません")
