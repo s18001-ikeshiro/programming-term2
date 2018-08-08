d77b85b4fa324546c315533a99497c87964d8213
s18001-ikeshiro <s18001@std.it-college.ac.jp>
2018-08-08 16:23:24 +0900

    20180808

---
+++ src/algo-p3/20180808_p01.py
@@ -0,0 +1,18 @@
+def print_hard():
+    """
+    グーしか出さない
+
+    Parameters
+    ----------
+    なし
+
+    Returns
+    -------
+    なし
+    """
+    print("グーを出しました")
+
+
+print_hard()
+
+help(print_hard)
---
+++ src/algo-p3/20180808_p02.py
@@ -0,0 +1,22 @@
+def print_hand(hand):
+    """
+    グーとパーを表示する
+
+    Paremeters
+    ----------
+    hand : str
+        じゃんけんの手
+
+    Returns
+    -------
+    なし
+    """
+    print(hand + "を出しました")
+
+
+print_hand("グー")
+
+
+print_hand("パー")
+
+help(print_hand)
