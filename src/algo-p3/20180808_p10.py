5cb9a2d00ceb4a3dee7d8ab9e62fdc69c2f3a57c
s18001-ikeshiro <s18001@std.it-college.ac.jp>
2018-08-09 16:52:34 +0900

    20180809の課題です。

---
+++ src/algo-p3/20180808_p10.py
@@ -0,0 +1,40 @@
+def validate(hand):
+    if hand < 0 or hand > 2:
+        return False
+    return True
+
+
+def print_hand(hand, name="ゲスト"):
+    hands = ["グー", "チョキ", "パー"]
+    print(name + "は" + hands[hand] + "を出しました")
+
+
+def judge(player, computer):
+    if player == computer:
+        return "引き分け"
+    elif player == 0 and computer == 1:
+        return "勝ち"
+    elif player == 1 and computer == 2:
+        return "勝ち"
+    elif player == 2 and computer == 0:
+        return "勝ち"
+    return "負け"
+
+
+print("じゃんけんをはじめます")
+player_name = input("名前を入力してください")
+print("何を出しますか？ (0: グー, 1: チョキ, 2: パー)")
+player_hand = int(input("数字で入力してください:"))
+
+if validate(player_hand):
+    computer_hand = 1
+    if player_name == "":
+        print_hand(player_hand)
+    else:
+        print_hand(player_hand, player_name)
+
+    print_hand(computer_hand, "コンピューター")
+    result = judge(player_hand, computer_hand)
+    print("結果は" + result + "です")
+else:
+    print("正しい数値を入力してください")
